5eb053db90172bf9abe74cba54f98df80e010fe2
Deepa-nshi <145038916+Deepa-nshi@users.noreply.github.com>
2023-10-15 14:26:16 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,30 @@
+const clock=document.querySelector(".clock");
+let sec=0,
+    min=0 ,
+    hr=0;
+let intervalId;
+
+const startstopwatch=()=>{
+  intervalId=setInterval(()=>{
+    if (sec<59){
+    sec++;
+    }
+    
+     else if(min>=59) 
+    {  hr++;
+        min=0;
+    }
+    else{
+        sec=0;
+        min++;
+    }
+    const hour=String(hr).padStart(2,"0");
+    const minutes=String(min).padStart(2,"0");
+    const seconds=String(sec).padStart(2,"0");
+       clock.innerText=(`${hour}:${minutes}:${seconds}`);
+  },1000);
+};
+
+const stopstopwatch=()=>{
+    clearInterval(intervalId);
+};
